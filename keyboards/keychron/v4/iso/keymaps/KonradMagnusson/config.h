95e56bb33aeddeaccaf5f0996c4715a3bdf9f5e7
Konrad Magnusson <konrad.magnusson@paradoxinteractive.com>
2025-05-19 11:38:39 +0200

    add keymap for my v4

---
+++ keyboards/keychron/v4/iso/keymaps/KonradMagnusson/config.h
@@ -0,0 +1,13 @@
+#define TAPPING_TERM 125
+
+#define LSPO_MOD KC_LSFT
+#define LSPO_KEY KC_8
+
+#define RSPC_MOD KC_RSFT
+#define RSPC_KEY KC_9
+
+#define LCPO_KEYS KC_LCTL, KC_RALT, KC_7
+#define RCPC_KEYS KC_RCTL, KC_RALT, KC_0
+
+#define LAPO_KEYS KC_LALT, KC_RALT, KC_8
+#define RAPC_KEYS KC_RALT, KC_RALT, KC_9
